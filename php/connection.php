ebbc10ff0923154e07dd3fba80f046d9629ccc5c
Hirunithathsara <hitunithathsaralokuthanthri@gmail.com>
2024-09-30 14:35:44 +0530

    change

--- php/connection.php
+++ php/connection.php
@@ -1,3 +1,13 @@
 <?php
+$servername ="localhost";
+$username="root";
+$password= "";
+$db_name ="school_login";
+$conn = new mysqli($servername,$username, $password, $db_name, 3307);
+
+if(conn->connect_error){
+    die("Connection failed".Sconn->connect_error);
+}
+echo "Successfull"
 
 ?>
