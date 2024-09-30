e77095ee59da885427464c0483faf852c260971a
Hirunithathsara <hitunithathsaralokuthanthri@gmail.com>
2024-09-30 12:57:38 +0530

    Change

--- php/signup.php
+++ php/signup.php
@@ -7,6 +7,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
+  <?php
+     include "navbar.php";
+     ?>
     <div id = "form">
         <h1>Signup Form</h1>
         <form name="form" action="signup.php" method= "POST">
