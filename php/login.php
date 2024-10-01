df0f1c525c37afd76a5f34351e5156ab650f9955
Hirunithathsara <hitunithathsaralokuthanthri@gmail.com>
2024-10-01 11:00:44 +0530

    change

--- php/login.php
+++ php/login.php
@@ -1,7 +1,8 @@
 <?php
 if(isset($_POST['submit'])){
-  $username = mysqli_real_escape_string($_POST['user']);
-  $username = mysqli_real_escape_string($_POST['pass']);
+  include "connection.php";
+  $username = mysqli_real_escape_string($conn, $_POST['user']);
+  $username = mysqli_real_escape_string($conn, $_POST['pass']);
 
   $sql = "select * from users where username = '$username' or email='$username' ";
   $result= mysqli_query($conn,$sql);
@@ -14,7 +15,7 @@ if(isset($_POST['submit'])){
   }
   else {
     echo '<script>
-    alert("Invalid username or password!!");
+    alert("Invalid username /emailor password!!");
     window.location.href= "login.php";
     </script>';
   }
@@ -28,6 +29,7 @@ if(isset($_POST['submit'])){
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href = "style.css">
   </head>
   <body>
   <?php
